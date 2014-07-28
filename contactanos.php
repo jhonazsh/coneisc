ef1adb88f75601964ffd7258eaeeb7a0038405f1
jhon medina <jhofe_s17@hotmail.com>
2014-07-28 17:50:53 -0500

    optimizando

---
+++ contactanos.php
@@ -0,0 +1,9 @@
+<?php include('base.php'); ?>
+
+<?php startblock('titulo'); ?>
+    XXII CONEISC 2014 | Contáctanos
+<?php endblock(); ?>
+
+<?php startblock('contenido'); ?>
+
+<?php endblock(); ?>
---
+++ trujillo.php
@@ -0,0 +1,314 @@
+<?php include('base.php'); ?>
+
+<?php startblock('titulo'); ?>
+    XXII CONEISC 2014 | Trujillo
+<?php endblock(); ?>
+
+<?php startblock('contenido'); ?>
+    <div class="banner banner-inscripcion">
+      <div class="container-banner">
+        <!-- Indicators -->
+          <div class="container">
+
+            <div class="row">
+              
+              <div class="col-sm-6">
+                <h1 class="titulo-principal">Trujillo CONEISC</h1>
+                <!--<p class="parrafo-principal color-blanco">¡No esperes el ultimo momento!</p>-->
+              </div>
+
+            </div>
+        </div>
+      </div>
+
+    </div>
+
+
+    <section>
+        <br>
+        <div class="container">
+                <ul class="nav nav-tabs trujillo-tabs" role="tablist">
+                  <li class="active"><a href="#home" role="tab" data-toggle="tab">Turismo</a></li>
+                  <li><a href="#profile" role="tab" data-toggle="tab">Hoteles</a></li>
+                  <li><a href="#messages" role="tab" data-toggle="tab">Restaurantes</a></li>
+                  <li><a href="#settings" role="tab" data-toggle="tab">Diversión</a></li>
+                </ul>
+                <br>
+                <div class="tab-content">
+
+                  <div class="tab-pane active" id="home">
+
+                        <div class="caja-trujillo">
+                            <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Huanchaco</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/huanchaco.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                         Huanchaco es el balneario más importante de la ciudad de Trujillo. En Huanchaco podrá apreciar también las embarcaciones de los caballitos de totora incrustadas en la arena reposando. A lo largo del malecón de Huanchaco se encuentra un gran número de restaurantes que ofrecen platos típicos de la zona a base de variados pescados y mariscos.
+                                         </p>
+                                     </div>
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                     <h3 class="text-center">Chan Chan</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/chanchan.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                         Chan Chan, que significa «la ciudad del sol» en qignam, la lengua hablada en la región, fue fundada en el año 850 por el pueblo chimú. Chan Chan no se parece a ninguna otra ciudad del continente. Se compone de nueve ciudadelas, construidas totalmente en adobe, una sucesión de complejos muros de barro de hasta doce metros de alto y seiscientos de largo..
+                                     </p>
+                                     </div>
+                                 </div>
+                         </div> 
+                        
+                        <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Plaza de armas</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/plazadearmas.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                         La Plaza de Armas de Trujillo o Plaza Mayor de Trujillo, es la plaza donde se realizó la fundación española de la ciudad de Trujillo, en la costa norte peruana. Es el principal escenario histórico de la ciudad de la era republicana. Se encuentra ubicada en la parte central del Centro histórico de Trujillo.
+                                         </p>
+                                     </div>
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                     <h3 class="text-center">Huaca del sol y la luna</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/huaca.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                        <p class="text-justify">
+                                            Las Huacas del Sol y de la luna es un complejo arqueológico ubicado en la costa norte del Perú, considerados como un santuario mochica constituido por un conjunto de monumentos situados a unos cinco kilómetros al sur deTrujillo, en el distrito de Moche.     
+                                        </p>
+                                     </div>
+                                 </div>
+                         </div> 
+
+                        
+                        </div>
+                        <br>
+                  </div>
+
+                  <div class="tab-pane" id="profile">
+                      <div class="caja-trujillo">
+                            <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Hotel Plaza Grau</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/hotelgrau.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                         Dirección: Jr Grau nro. 722 Centro Histórico
+                                        Servicios: WiFi, Tv Cable, Lavandería, Bar, Restaurante
+                                        Telefonos:
+                                        Tlf: 51 (044)208 313
+                                        Rpc: 965382353
+                                        Rpm: 965907037
+                                        Página web: http://hotelplazagrau.pe
+                                        </p>
+                                     </div>
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                     <h3 class="text-center">Hotel Chimú</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/hotelchimu.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                        <p class="text-justify">
+                                            Dirección: Av. Coronado Oquillas
+                                            Servicios: Tv cable, agua caliente, WiFi
+                                            Tarifas:
+                                            Simple s/80
+                                            Doble s/130
+                                            Triple s/150
+                                        </p>
+                                     </div>
+                                 </div>
+                         </div> 
+                        
+                        <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Hotel San Andrés</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/hotelsanandres.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                            Direccion: Av. Juan Pablo II 157
+                                            Servicios: Tv Cable, WiFi, piscina
+                                            Precios:
+                                            Simple s/125
+                                            doble s/165
+                                            triple s/185
+                                         </p>
+                                     </div>
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                     <h3 class="text-center">Hostal el Frayle</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/hotelfrayle.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                        <p class="text-justify">
+                                            Dirección: Av. Recreo
+                                            Servicios: Teléfono, Tv Cable, Lavandería
+                                            Tarifas:
+                                            Simple s/80
+                                            Doble s/120
+                                            Triple s/160     
+                                        </p>
+                                     </div>
+                                 </div>
+                         </div> 
+
+                        <div class="centrar">
+                            <a class="btn btn-azul" type="button" href="descargas/trujillo/Hoteles.pdf" target="_blank">
+                                Hoteles
+                            </a>
+                        </div>
+                        </div>
+                        <br>
+                  </div>
+                  <div class="tab-pane" id="messages">
+                      <div class="caja-trujillo">
+                            <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Cebichería El Marisco Del Amor</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/cebicheria.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                         Dirección: Av. Jesús de Nazareth, 510 Urb. San Nicolás
+                                        Referencia: Frente a la puerta lateral de la UNT (CIDUNT)
+                                        Teléfono: (044)20-1053
+                                        </p>
+                                     </div>
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                     <h3 class="text-center">Pizzería Pizanino</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/pizza.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                        <p class="text-justify">
+                                            Dirección: Av. Juan Pablo II 183, Urb. San Andrés
+                                            Referencia: Al frente de la puerta principal de la UNT
+                                        </p>
+                                     </div>
+                                 </div>
+                         </div> 
+                        
+                        <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Chifa Hing Da</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/chifa.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                            Direccion: Av. Jesús de Nazareth #215
+                                            Referencia: cerca av. España que rodea al centro cívico
+                                            Menú: S/.4.50 – S/.8.50
+                                         </p>
+                                     </div>
+                                 </div>
+                         </div> 
+                         <div class="centrar">
+                            <a class="btn btn-azul" type="button" href="descargas/trujillo/Restaurantes.pdf" target="_blank">
+                                Restaurantes
+                            </a>
+                        </div>
+                        </div>
+                        <br>
+                  </div>
+                  <div class="tab-pane" id="settings">
+                      <div class="caja-trujillo">
+                            <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Mall aventura plaza</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/mall.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                         Ven a Mall Aventura Plaza Trujillo y disfruta de los mejores servicios en moda, cultura, cine, entretención y Motor Plaza. Av. America Oeste 750 Urb. El Ingenio.
+                                        </p>
+                                     </div>
+                                 </div>
+
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                     <h3 class="text-center">Real plaza</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/real.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                        <p class="text-justify">
+                                            En Trujillo como las principales ciudades cuenta con centros comerciales con reconocidas tiendas. En las cuales podrás tener un momento agradable entre amigos o personas que recién conozcas ofreciéndote cines, restaurantes, cafés, discotecas, y más. Ubicado en la intersección de Av. Fátima con prolongación Cesar Vallejo 1345
+                                        </p>
+                                     </div>
+                                 </div>
+                         </div> 
+                        
+                        <div class="row">
+                                 <div class="col-xs-12 col-sm-6 col-md-6">
+                                    <h3 class="text-center">Jardín Botánico</h3>
+                                    <div class="img-centrar-trujillo">
+                                        <img src="img/trujillo/jardin.jpg" class="img-trujillo" alt="">
+                                    </div>
+                                     
+                                     <br>
+                                     <div class="text-trujillo">
+                                         <p class="text-justify">
+                                            Este parque de 2.500 metros cuadrados es en la actualidad uno de los lugares favoritos de los enamorados, universitarios, amantes de la naturaleza, estudiosos de las especies animales y vegetales, así como de quienes practican el yoga o buscan la inspiración para dedicarse a las actividades artísticas. Avenida América Sur, Trujillo, Perú
+                                         </p>
+                                     </div>
+                                 </div>
+                         </div> 
+                        </div>
+                        <br>
+                  </div>
+                </div>
+
+
+        </div>
+    </section>
+<?php endblock(); ?>
+
