813c72208e638e1e16de4d8cec500afb285b7914
jhon medina <jhofe_s17@hotmail.com>
2014-07-29 17:24:50 -0500

    cambiando auspiciadores

--- index.php
+++ index.php
@@ -136,10 +136,10 @@
           <img src="img/auspiciadores/cjavalogo.png" class="img-responsive">
           <img src="img/auspiciadores/mpclogo.png" class="img-responsive">
           <img src="img/auspiciadores/integrity.png" class="img-responsive">
-          <img src="img/auspiciadores/plazagrau.png" class="img-responsive">
           <img src="img/auspiciadores/devacademy.png" class="img-responsive">
           <img src="img/auspiciadores/Logo_fedoralogo.png" class="img-responsive">
           <img src="img/auspiciadores/mejorandola_logo.png" class="img-responsive">
+          <img src="img/auspiciadores/plazagrau.png" class="img-responsive">
         </div>
       </section>
 
